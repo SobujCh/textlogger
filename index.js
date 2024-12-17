e8b27910df7843a22bde123edcf0e1619a8fbec2
Sobuj Chowdhury <sobujos@gmail.com>
2024-12-17 11:12:16 +0600

    Update version to 1.1.3 and ensure file is initialized asynchronously before appending data

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function debuglog(...txt){
 }
 async function appendToFile(filePath, data) {
     try {
-        if (!fileExists(filePath)){ fs.writeFile(filePath, ''); }
+        if (!fileExists(filePath)){ await fs.writeFile(filePath, ''); }
         await fs.appendFile(filePath, data);
     } catch (error) {
         console.error('Error appending to file', error);
